dd3857b861c03e01d2715a18d1f01a5855fa59ff
Christian Chukwu <christianchukwu@Christians-MacBook-Pro-2.local>
2024-02-06 13:07:40 -0700

    provisioned the loadbalancer that routes traffic to cloud run with required IAM

--- cloud-run/main.tf
+++ cloud-run/main.tf
@@ -1,46 +1,5 @@
 ##rns.id/cloud-run/main.tf
 
-#data "terraform_remote_state" "vpc" {
-#  backend = "gcs"
-#  bucket  = "rns-state-devops-rns"
-#  config = {
-#    prefix  = "terraform/state/gcp-vpc"
-#  }
-#}
-
-## Serverless VPC Access connector
-#resource "google_vpc_access_connector" "serverless_connector" {
-#  name          = "serverless-connector"
-#  region        = var.region
-#  network       = data.terraform_remote_state.vpc.outputs.vpc_id
-#  ip_cidr_range = "10.8.0.0/28"
-#}
-
-## Cloud Run service
-#resource "google_cloud_run_service" "cloud_run_service" {
-#  name     = "rns-service"
-#  location = var.region
-#
-#  template {
-#    spec {
-#      containers {
-#        image = "gcr.io/cloudrun/hello"
-#      }
-#    }
-#  }
-
-#  traffic {
-#    percent         = 100
-#    latest_revision = true
-#  }
-
-#  autogenerate_revision_name = true
-#
-#  depends_on = [
-#    google_vpc_access_connector.serverless_connector
-#  ]
-#}
-
 data "terraform_remote_state" "vpc" {
   backend = "gcs"
   config = {
